01123305e0c7e6f7115a1e0ea07e54a64226a95a
Teodor Tenchev <teodortenchev@gmail.com>
2020-07-13 20:20:57 +0400

    Update index.js

--- src/components/origamis/index.js
+++ src/components/origamis/index.js
@@ -36,7 +36,6 @@ class Origamis extends Component {
     }
 
     render() {
-
         return (
             <div className={styles.container}>
                 <h1 className={styles.title}>Origamis</h1>
