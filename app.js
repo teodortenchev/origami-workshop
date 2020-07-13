c134e54d41e89022622c97bb9eb689edff782dd5
Teodor Tenchev <teodortenchev@gmail.com>
2020-07-13 20:28:58 +0400

    fix: app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import styles from './app.module.css';
 import Aside from './components/aside';
 import Origamis from './components/origamis';
 
-const App = () => {
+const Application = () => {
   return (
     <div className={styles.app}>
      <Header />
@@ -16,4 +16,4 @@ const App = () => {
   );
 }
 
-export default App;
+export default Application;
