c182db41a98b5fa8ad22bad0a8444b2ebd584b44
Teodor Tenchev <teodortenchev@gmail.com>
2020-07-14 15:48:38 +0400

    feat:implemented-footer

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import Header from './components/header';
 import styles from './app.module.css';
 import Aside from './components/aside';
 import Origamis from './components/origamis';
+import Footer from './components/footer';
 
 const Application = () => {
   return (
@@ -12,8 +13,10 @@ const Application = () => {
        <Aside />
        <Origamis />
      </div>
+     <Footer />
     </div>
-  );
+  )
 }
+
 
 export default Application;
